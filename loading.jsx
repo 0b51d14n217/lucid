e7222aad8d31843441d25d89601f0302e3cefc24
Dhruv <59387969+druvdub@users.noreply.github.com>
2021-09-30 17:53:37 +0100

    Create loading.jsx

---
+++ loading.jsx
@@ -0,0 +1,34 @@
+const loading = {
+  width: 1920,
+  height: 1080,
+  backgroundColor: "#1b1b1b",
+  overflow: "hidden",
+}
+
+
+<Video
+  loopType="noLoop"
+  posterEnabled={false}
+  srcFile={ezgif.com-gif-maker.mp4}
+  srcType="Upload"
+  // Using default values:
+  backgroundColor="rgba(0,0,0,0)"
+  canvasPlay={false}
+  controls={false}
+  muted={true}
+  objectFit="cover"
+  onClick={undefined}
+  onEnd={undefined}
+  onMouseEnter={undefined}
+  onMouseLeave={undefined}
+  onPause={undefined}
+  onPlay={undefined}
+  onSeeked={undefined}
+  playing={true}
+  poster={undefined}
+  preload="auto"
+  restartOnEnter={false}
+  srcUrl="https://assets.mixkit.co/videos/preview/mixkit-ice-cream-glass-of-red-soda-5094-small.mp4"
+  startTime={0}
+  volume={25}
+/>
