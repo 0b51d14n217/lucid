5cddb32055c08d2a46a5c0ed0b15bb75a9f05276
Arnav Sharma <84272970+0b51d14n217@users.noreply.github.com>
2021-09-30 22:35:11 +0530

    Create login.tsx

---
+++ login.tsx
@@ -0,0 +1,79 @@
+const loginSignup = {
+  width: 1920,
+  height: 1080,
+  backgroundColor: "#1b1b1b",
+  overflow: "hidden",
+}
+
+const loginDetails = {
+  boxSizing: "border-box",
+  width: 570,
+  height: 290,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "165px 177px 165px 177px",
+  overflow: "visible",
+  borderRadius: 20,
+<Input
+  placeholder="Email/Username"
+  {/* Using default values */}
+  backgroundColor="#EBEBEB"
+  border="rgba(0,0,0,0)"
+  borderWidth={1}
+  caretColor="#333"
+  disabled={false}
+  enableLimit={false}
+  focusColor="#09F"
+  focused={false}
+  font={false}
+  fontFamily=""
+  fontSize={16}
+  isRTL={false}
+  keyboard=""
+  lineHeight={1.4}
+  maxLength={10}
+  multiLine={false}
+  onBlur={undefined}
+  onChange={undefined}
+  onFocus={undefined}
+  onSubmit={undefined}
+  password={false}
+  placeholderColor="#aaa"
+  textAlign="left"
+  textColor="#333"
+  truncate={false}
+  value=""
+/>
+<Input
+  password={true}
+  placeholder="Password"
+  {/* Using default values */}
+  backgroundColor="#EBEBEB"
+  border="rgba(0,0,0,0)"
+  borderWidth={1}
+  caretColor="#333"
+  disabled={false}
+  enableLimit={false}
+  focusColor="#09F"
+  focused={false}
+  font={false}
+  fontFamily=""
+  fontSize={16}
+  isRTL={false}
+  keyboard=""
+  lineHeight={1.4}
+  maxLength={10}
+  multiLine={false}
+  onBlur={undefined}
+  onChange={undefined}
+  onFocus={undefined}
+  onSubmit={undefined}
+  placeholderColor="#aaa"
+  textAlign="left"
+  textColor="#333"
+  truncate={false}
+  value=""
+/>
+}
