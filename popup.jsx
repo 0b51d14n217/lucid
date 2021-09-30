cbdc48d7e4270090141bd7371d13bafe8d0a4d16
Dhruv <59387969+druvdub@users.noreply.github.com>
2021-09-30 18:27:10 +0100

    Create popup.jsx

---
+++ popup.jsx
@@ -0,0 +1,48 @@
+const joiningRoomPopup = {
+  width: 926,
+  height: 336,
+  backgroundColor: "#0a0c0f",
+  overflow: "hidden",
+  borderRadius: 50,
+  
+  <Button
+  borderRadius={15}
+  defaultBackground="rgb(27, 27, 27)"
+  fontSize={20}
+  hoverBackground="rgb(53, 143, 194)"
+  onClick={Transition Back}
+  pressedBackground="rgb(27, 27, 27)"
+  text="Cancel"
+  variant="Pressed"
+  // Using default values:
+  alignment="center"
+  defaultTextColor="#ffffff"
+  disabled={false}
+  disabledBackground="#f3f3f3"
+  disabledTextColor="#AAAAAA"
+  font={false}
+  fontFamily=""
+  hoverTextColor="#ffffff"
+  onMouseEnter={undefined}
+  onMouseLeave={undefined}
+  pressedTextColor="#ffffff"
+  shadow={false}
+  whileHoverScale={1.1}
+  whileTapScale={0.95}
+/>
+const text = {
+  width: 804,
+  height: 173,
+  overflow: "hidden",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontFamily: `"Poppins", serif`,
+  color: "#ffffff",
+  fontSize: 36,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+}
+}
