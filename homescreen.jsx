7396e738278727addef478f6c99b446a04acdadb
Dhruv <59387969+druvdub@users.noreply.github.com>
2021-09-30 18:22:15 +0100

    Update homescreen.jsx
    shortened code

--- homescreen.jsx
+++ homescreen.jsx
@@ -1,1 +1,201 @@
-
+const dellXPS = {
+  width: 1920,
+  height: 1080,
+  backgroundColor: "#1b1b1b",
+  overflow: "hidden",
+  
+  <SwitchToStateAction
+  children={undefined}
+  onHoverEndAction="specific"
+  onHoverStartAction="specific"
+  onHoverStartSpecificIndex={1}
+  // Using default values:
+  afterDelayAction="unset"
+  afterDelayDelay={0}
+  afterDelaySpecificIndex={0}
+  afterDelaySpecificName=""
+  onDoubleTapAction="unset"
+  onDoubleTapSpecificIndex={0}
+  onDoubleTapSpecificName=""
+  onDragEndAction="unset"
+  onDragEndSpecificIndex={0}
+  onDragEndSpecificName=""
+  onDragStartAction="unset"
+  onDragStartSpecificIndex={0}
+  onDragStartSpecificName=""
+  onHoverEndSpecificIndex={0}
+  onHoverEndSpecificName=""
+  onHoverStartSpecificName=""
+  onKeyDownAction="unset"
+  onKeyDownKey=""
+  onKeyDownSpecificIndex={0}
+  onKeyDownSpecificName=""
+  onKeyUpAction="unset"
+  onKeyUpKey=""
+  onKeyUpSpecificIndex={0}
+  onKeyUpSpecificName=""
+  onLongPressAction="unset"
+  onLongPressDuration={0.5}
+  onLongPressSpecificIndex={0}
+  onLongPressSpecificName=""
+  onTapAction="unset"
+  onTapCancelAction="unset"
+  onTapCancelSpecificIndex={0}
+  onTapCancelSpecificName=""
+  onTapSpecificIndex={0}
+  onTapSpecificName=""
+  onTapStartAction="unset"
+  onTapStartSpecificIndex={0}
+  onTapStartSpecificName=""
+  target="sharedSwitch"
+  targetType="named"
+/>
+   <Switch
+  ​children={[…]}
+ ​// Using default values:
+ ​afterDelayAction="unset"
+ ​afterDelayDelay={0}
+ ​afterDelaySpecificIndex={0}
+ ​afterDelaySpecificName=""
+ ​autoAssignIdentifier={false}
+ ​customEase="0.25, 0.1, 0.25, 1"
+ ​damping={45}
+ ​delayChildren={0}
+ ​duration={0.3}
+ ​ease="easeOut"
+ ​enterCustomEase="0.25, 0.1, 0.25, 1"
+ ​enterDamping={45}
+ ​enterDuration={0.3}
+ ​enterEase="easeOut"
+ ​enterMass={1}
+ ​enterStiffness={500}
+ ​enterTransition="enterdissolve"
+ ​enterTransitionConfigType="default"
+ ​enterTransitionType="spring"
+ ​exitCustomEase="0.25, 0.1, 0.25, 1"
+ ​exitDamping={45}
+ ​exitDuration={0.3}
+ ​exitEase="easeOut"
+ ​exitMass={1}
+ ​exitStiffness={500}
+ ​exitTransition="exitdissolve"
+ ​exitTransitionConfigType="default"
+ ​exitTransitionType="spring"
+ ​identifier="sharedSwitch"
+ ​initialState={0}
+ ​isInteractive={false}
+ ​mass={1}
+ ​onDoubleTapAction="unset"
+ ​onDoubleTapSpecificIndex={0}
+ ​onDoubleTapSpecificName=""
+ ​onDragEndAction="unset"
+ ​onDragEndSpecificIndex={0}
+ ​onDragEndSpecificName=""
+ ​onDragStartAction="unset"
+ ​onDragStartSpecificIndex={0}
+ ​onDragStartSpecificName=""
+ ​onHoverEndAction="unset"
+ ​onHoverEndSpecificIndex={0}
+ ​onHoverEndSpecificName=""
+ ​onHoverStartAction="unset"
+ ​onHoverStartSpecificIndex={0}
+ ​onHoverStartSpecificName=""
+ ​onKeyDownAction="unset"
+ ​onKeyDownKey=""
+ ​onKeyDownSpecificIndex={0}
+ ​onKeyDownSpecificName=""
+ ​onKeyUpAction="unset"
+ ​onKeyUpKey=""
+ ​onKeyUpSpecificIndex={0}
+ ​onKeyUpSpecificName=""
+ ​onLongPressAction="unset"
+ ​onLongPressDuration={0.5}
+ ​onLongPressSpecificIndex={0}
+ ​onLongPressSpecificName=""
+ ​onTapAction="unset"
+ ​onTapCancelAction="unset"
+ ​onTapCancelSpecificIndex={0}
+ ​onTapCancelSpecificName=""
+ ​onTapSpecificIndex={0}
+ ​onTapSpecificName=""
+ ​onTapStartAction="unset"
+ ​onTapStartSpecificIndex={0}
+ ​onTapStartSpecificName=""
+ ​overflow={true}
+ ​staggerChildren={0}
+ ​stiffness={500}
+ ​transition="instant"
+ ​transitionConfigType="default"
+ ​transitionType="spring"
+/>
+   const round = {
+  width: 20,
+  height: 20,
+  backgroundColor: "#ffbb00",
+  overflow: "hidden",
+  borderRadius: 100,
+}
+const scroll_1 = {
+  width: 343,
+  height: 800,
+}
+const files = {
+  width: "auto", /* 107px */
+  height: "auto", /* 60px */
+  overflow: "visible",
+  whiteSpace: "pre",
+  fontWeight: 100,
+  fontStyle: "normal",
+  fontFamily: `"Poppins", serif`,
+  color: "#ffffff",
+  fontSize: 50,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+}
+const text = {
+  width: "auto", /* 299px */
+  height: "auto", /* 25px */
+  overflow: "visible",
+  whiteSpace: "pre",
+  fontWeight: 100,
+  fontStyle: "normal",
+  fontFamily: `"Poppins", serif`,
+  color: "#ffffff",
+  fontSize: 21,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+}
+<Progress
+  animateFromStart={true}
+  lineHeight={16}
+  progress={20}
+  progressColor="rgb(5, 188, 255)"
+  progressEnd={40}
+  roundCap={false}
+  trackColor="rgb(46, 46, 46)"
+  type="Line"
+  // Using default values:
+  borderRadius={5}
+  onClick={undefined}
+  onComplete={undefined}
+  onMouseEnter={undefined}
+  onMouseLeave={undefined}
+  padding={5}
+  shouldAnimate={true}
+  shouldAnimateTransition={true}
+  strokeWidth={5}
+/>
+    const frame = {
+  width: 736,
+  height: 723,
+  backgroundColor: "#212121",
+  overflow: "visible",
+  borderRadius: 45,
+}
+const sideBar = {
+  width: 398,
+  height: 1080,
+  backgroundColor: "#141414",
+  overflow: "hidden",
+}
+}
