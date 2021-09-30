4738baaf5661802e35b8717242ab5ae615748575
Dhruv <59387969+druvdub@users.noreply.github.com>
2021-09-30 18:24:24 +0100

    Create callscreen.jsx

---
+++ callscreen.jsx
@@ -0,0 +1,46 @@
+const callScreen = {
+  width: 1920,
+  height: 1080,
+  backgroundColor: "#1b1b1b",
+  overflow: "hidden",
+  
+  const frame = {
+  width: 110,
+  height: 173,
+  backgroundColor: "rgba(5, 188, 255, 0.7)",
+  overflow: "visible",
+  borderRadius: 35,
+  }
+
+  const frame = {
+  width: 110,
+  height: 173,
+  backgroundColor: "rgba(255, 71, 120, 0.7)",
+  overflow: "visible",
+  borderRadius: 35,
+  }
+  
+  const frame = {
+  width: 110,
+  height: 173,
+  backgroundColor: "rgba(5, 255, 168, 0.7)",
+  overflow: "visible",
+  borderRadius: 35,
+  }
+  const callInitial = {
+  width: 398,
+  height: 512,
+  backgroundColor: "#0a0c0f",
+  overflow: "hidden",
+}
+  const videoCallBG = {
+  boxSizing: "border-box",
+  width: 1499,
+  height: 1144,
+  backgroundColor: "rgba(0, 121, 173, 0.1)",
+  overflow: "hidden",
+  borderRadius: 170,
+  border: "2px solid #ffffff",
+}
+  
+}
