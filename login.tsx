73edfb62e07900ac9d6eb5abe85ccbcf89ca5f6f
Arnav Sharma <84272970+0b51d14n217@users.noreply.github.com>
2021-09-30 22:39:58 +0530

    Create login.tsx

--- login.tsx
+++ login.tsx
@@ -16,64 +16,103 @@ const loginDetails = {
   padding: "165px 177px 165px 177px",
   overflow: "visible",
   borderRadius: 20,
-  <Input
-    placeholder="Email/Username"
-    {/* Using default values */}
-    backgroundColor="#EBEBEB"
-    border="rgba(0,0,0,0)"
-    borderWidth={1}
-    caretColor="#333"
-    disabled={false}
-    enableLimit={false}
-    focusColor="#09F"
-    focused={false}
-    font={false}
-    fontFamily=""
-    fontSize={16}
-    isRTL={false}
-    keyboard=""
-    lineHeight={1.4}
-    maxLength={10}
-    multiLine={false}
-    onBlur={undefined}
-    onChange={undefined}
-    onFocus={undefined}
-    onSubmit={undefined}
-    password={false}
-    placeholderColor="#aaa"
-    textAlign="left"
-    textColor="#333"
-    truncate={false}
-    value=""
-   />
-  <Input
-    password={true}
-    placeholder="Password"
-    {/* Using default values */}
-    backgroundColor="#EBEBEB"
-    border="rgba(0,0,0,0)"
-    borderWidth={1}
-    caretColor="#333"
-    disabled={false}
-    enableLimit={false}
-    focusColor="#09F"
-    focused={false}
-    font={false}
-    fontFamily=""
-    fontSize={16}
-    isRTL={false}
-    keyboard=""
-    lineHeight={1.4}
-    maxLength={10}
-    multiLine={false}
-    onBlur={undefined}
-    onChange={undefined}
-    onFocus={undefined}
-    onSubmit={undefined}
-    placeholderColor="#aaa"
-    textAlign="left"
-    textColor="#333"
-    truncate={false}
-    value=""
-   />
+<Input
+  placeholder="Email/Username"
+  {/* Using default values */}
+  backgroundColor="#EBEBEB"
+  border="rgba(0,0,0,0)"
+  borderWidth={1}
+  caretColor="#333"
+  disabled={false}
+  enableLimit={false}
+  focusColor="#09F"
+  focused={false}
+  font={false}
+  fontFamily=""
+  fontSize={16}
+  isRTL={false}
+  keyboard=""
+  lineHeight={1.4}
+  maxLength={10}
+  multiLine={false}
+  onBlur={undefined}
+  onChange={undefined}
+  onFocus={undefined}
+  onSubmit={undefined}
+  password={false}
+  placeholderColor="#aaa"
+  textAlign="left"
+  textColor="#333"
+  truncate={false}
+  value=""
+/>
+<Input
+  password={true}
+  placeholder="Password"
+  {/* Using default values */}
+  backgroundColor="#EBEBEB"
+  border="rgba(0,0,0,0)"
+  borderWidth={1}
+  caretColor="#333"
+  disabled={false}
+  enableLimit={false}
+  focusColor="#09F"
+  focused={false}
+  font={false}
+  fontFamily=""
+  fontSize={16}
+  isRTL={false}
+  keyboard=""
+  lineHeight={1.4}
+  maxLength={10}
+  multiLine={false}
+  onBlur={undefined}
+  onChange={undefined}
+  onFocus={undefined}
+  onSubmit={undefined}
+  placeholderColor="#aaa"
+  textAlign="left"
+  textColor="#333"
+  truncate={false}
+  value=""
+/>
+const forgotPassword = {
+  width: 200,
+  height: 26,
+  overflow: "hidden",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  fontFamily: `"Inter", sans-serif`,
+  color: "#ffffff",
+  fontSize: 16,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+}
+<Button
+  borderRadius={30}
+  defaultBackground="rgb(53, 143, 194)"
+  font={true}
+  fontFamily="inter"
+  fontSize={24}
+  onClick={Interaction}
+  padding={8}
+  text="Login"
+  whileTapScale={1.2}
+  {/* Using default values */}
+  alignment="center"
+  defaultTextColor="rgb(255, 255, 255)"
+  disabled={false}
+  disabledBackground="#f3f3f3"
+  disabledTextColor="#AAAAAA"
+  hoverBackground="#0077FF"
+  hoverTextColor="#ffffff"
+  onMouseEnter={undefined}
+  onMouseLeave={undefined}
+  pressedBackground="#0088FF"
+  pressedTextColor="#ffffff"
+  shadow={false}
+  variant="Default"
+  whileHoverScale={1.1}
+/>
 }
